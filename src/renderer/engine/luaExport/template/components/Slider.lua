35210482026d657fd86d421eeb745e1ab7c99fa7
Mohamed Mahmodi <mohammed5mmc@gmail.com>
2026-03-20 01:59:29 +0300

    added readme

--- src/renderer/engine/luaExport/template/components/Slider.lua
+++ src/renderer/engine/luaExport/template/components/Slider.lua
@@ -14,6 +14,25 @@ function Slider:new(name, props)
     return obj
 end
 
+function Slider:getValue() return self.value end
+function Slider:setValue(val) self.value = math.max(self.from, math.min(self.to, math.floor(val + 0.5))) end
+
+function Slider:checkTouch(x, y, button)
+    if self.visible == false then return false end
+
+    if x >= self.x and x < self.x + self.width and y >= self.y and y < self.y + self.height then
+        self.value =
+            self.from
+            + (self.to - self.from)
+            * ((self.orientation:match("^" .. "v")
+                and (self.orientation == "vbtt" and 100 - math.floor((y - self.y) * 100 / (self.height - 1) + 0.5) or math.floor((y - self.y) * 100 / (self.height - 1) + 0.5))
+                or (self.orientation == "hrtl" and 100 - math.floor((x - self.x) * 100 / (self.width - 1) + 0.5) or math.floor((x - self.x) * 100 / (self.width - 1) + 0.5))) / 100)
+        self.value = math.max(self.from, math.min(self.to, math.floor(self.value + 0.5)))
+
+        return true;
+    end
+end
+
 function Slider:drawElement()
     local percentValue = math.floor(((self.value - self.from) * 100 / (self.to - self.from)) + 0.5);
 
@@ -27,7 +46,9 @@ function Slider:drawElement()
     if self.orientation:match("^" .. "v") then
         term.setBackgroundColor(self.filledColor)
         for j = 0, math.floor(self.height * percentValue / 100 + 0.5) - 1 do
-            term.setCursorPos(self.x, self.y + (self.orientation == "vbtt" and math.floor(self.height * (100 - percentValue) / 100 + 0.5) or 0) + j)
+            term.setCursorPos(self.x,
+                self.y + (self.orientation == "vbtt" and math.floor(self.height * (100 - percentValue) / 100 + 0.5) or 0) +
+                j)
             term.write(string.rep(" ", self.width))
         end
 
