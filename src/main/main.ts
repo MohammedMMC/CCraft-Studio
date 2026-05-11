57c8bb82dc4fa587086c1995f3bae79108859468
Mohamed Mahmodi <mohammed5mmc@gmail.com>
2026-05-11 15:17:08 +0300

    v0.5.0-alpha

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,14 +11,9 @@ const useSafeGraphics =
   process.env.CCRAFT_DISABLE_GPU === '1';
 
 if (isLinux) {
-  process.env.ELECTRON_OZONE_PLATFORM_HINT = 'x11';
-  process.env.OZONE_PLATFORM = 'x11';
-  process.env.WAYLAND_DISPLAY = '';
-  app.commandLine.appendSwitch('ozone-platform', 'x11');
-  app.commandLine.appendSwitch('ozone-platform-hint', 'x11');
-  app.commandLine.appendSwitch('no-sandbox');
-  app.commandLine.appendSwitch('enable-features', 'UseOzonePlatform');
-  app.commandLine.appendSwitch('disable-features', 'WaylandWindowDecorations,Wayland');
+  app.commandLine.appendSwitch('ozone-platform-hint', 'auto');
+  app.commandLine.appendSwitch('disable-gpu-sandbox');
+  app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder');
 }
 
 if (useSafeGraphics) {
